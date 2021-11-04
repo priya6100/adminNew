c152b0db63b3c397676a1bdab68f674c4665d4fe
priya6100 <priyaranjan@grapsnextsocial.com>
2021-11-04 14:21:17 +0530

    initall comnewtwo:new

---
+++ src/urlConfig.js
@@ -0,0 +1,7 @@
+const baseUrl =  "https://soulbackend.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (fileName) =>{
+
+    return `${baseUrl}/public/${fileName}`;
+}
